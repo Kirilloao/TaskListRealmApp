b67a83481edda170de4fab09f598cf7721ae320f
Kirilloao <kirilltr@list.ru>
2023-07-09 12:49:38 +0200

    Delete Storyboard, create scene

--- TaskListRealmApp/Controllers/TaskListsViewController.swift
+++ TaskListRealmApp/Controllers/TaskListsViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class TaskListsViewController: UITableViewController {
 
+    // MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
 
 
