4cc4e37ca935fbb314c044b23d00e30dd9b94e42
Kirilloao <kirilltr@list.ru>
2023-07-09 12:41:33 +0200

    Initial Commit

---
+++ TaskListRealmApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskListRealmApp
+//
+//  Created by Kirill Taraturin on 09.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
